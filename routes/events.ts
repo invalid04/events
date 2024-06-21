a0d722a43b29dba068ee92a9fc623c7c6e16a62b
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-06-21 01:35:02 -0500

    getting and posting with dummy data

--- routes/events.ts
+++ routes/events.ts
@@ -1,9 +1,42 @@
 import { Hono } from 'hono'
 
+type Event = {
+    id: number,
+    title: string,
+    description: string,
+    date: string,
+    time: string,
+    location: string,
+    maxAttendance: string,
+}
+
+const fakeEvents: Event[] = [
+    {
+        "id": 1,
+        "title": "Movie Night",
+        "description": "Screening of Casablanca",
+        "date": "2024-07-12",
+        "time": "19:00",
+        "location": "Central Park",
+        "maxAttendance": "100"
+      },
+      {
+        "id": 2,
+        "title": "Board Game Club",
+        "description": "Bring your favorite game to share!",
+        "date": "2024-06-28",
+        "time": "18:00",
+        "location": "Community Center",
+        "maxAttendance": "15"
+      }
+]
+
 export const eventsRoute = new Hono()
 .get('/', (c) => {
-    return c.json({ events: [] })
+    return c.json({ events: fakeEvents })
 })
-.post('/', (c) => {
-    return c.json({})
+.post('/', async (c) => {
+    const event = await c.req.json()
+    console.log(event)
+    return c.json(event)
 })
