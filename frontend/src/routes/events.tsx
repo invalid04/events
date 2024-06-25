42edbd647f32e377e10eb01907a3914b951a421c
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-06-25 00:44:10 -0500

    query for getting all events

--- frontend/src/routes/events.tsx
+++ frontend/src/routes/events.tsx
@@ -1,17 +1,31 @@
 import { createFileRoute } from '@tanstack/react-router'
 import EventCard from '@/components/Custom/EventCard'
+import { api } from '@/lib/api'
+import { useQuery } from '@tanstack/react-query'
 
 export const Route = createFileRoute('/events')({
   component: Events,
 })
 
+async function getAllEvents() {
+  const res = await api.events.$get()
+  if (!res.ok) {
+    throw new Error('server-error')
+  }
+  const data = await res.json()
+  return data
+}
+
 function Events() {
+
+  const { isPending, error, data } = useQuery({
+    queryKey: ['get-all-events'],
+    queryFn: getAllEvents,
+  }) 
+
   return (
     <div>
-      <EventCard />
-      <EventCard />
-      <EventCard />
-      <EventCard />
+      {isPending ? '...' : JSON.stringify(data, null, 2)}
     </div>
   )
 }
