5cb5b65050ac5dd2be099f77fa85d3e6c6626645
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-06-25 01:34:33 -0500

    finished rough form design

--- frontend/src/components/Custom/CreateEvent.tsx
+++ frontend/src/components/Custom/CreateEvent.tsx
@@ -1,11 +1,28 @@
 import { Input } from "../ui/input"
 import { Label } from "../ui/label"
+import { Textarea } from "../ui/textarea"
+import { Button } from '../ui/button'
 
 export default function CreateEvent() {
   return (
-    <form>
-        <Label htmlFor='title'>Title</Label>
-        <Input type='string' id='title' placeholder='Title' />
+    <form className='max-w-xl m-auto flex flex-col gap-2'>
+        <Input type='text' id='title' placeholder='Title' />
+
+        <Textarea id='desc' placeholder='Type a description' />
+
+        <Input type='text' id='date' placeholder='Date' />
+
+        <Input type='text' id='time' placeholder='Time' />
+
+        <Input type='text' id='location' placeholder='Location' />
+
+        <Input type='text' id='number' placeholder='no. of People' />
+
+        <Button
+            type='submit'
+        >
+            Create
+        </Button>
     </form>
   )
 }
