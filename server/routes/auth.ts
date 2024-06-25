59091c48a9b9dd028616f703a741732bd0d9a719
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-06-25 02:56:30 -0500

    added logout route

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -11,3 +11,12 @@ export const authRoute = new Hono()
     const registerUrl = await kindeClient.register(sessionManager(c))
     return c.redirect(registerUrl.toString())
 })
+.get('/callback', async (c) => {
+    const url = new URL(c.req.url)
+    await kindeClient.handleRedirectToApp(sessionManager(c), url)
+    return c.redirect('/')
+})
+.get('/logout', async (c) => {
+    const logoutUrl = await kindeClient.logout(sessionManager(c))
+    return c.redirect(logoutUrl.toString())
+})
