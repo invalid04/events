a0464518b1e0567f89a3e0a4cef253a2ed87d90f
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-06-26 00:39:26 -0500

    passed context

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -12,8 +12,10 @@ import { RouterProvider, createRouter } from '@tanstack/react-router'
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
 
+const queryClient = new QueryClient()
+
 // Create a new router instance
-const router = createRouter({ routeTree })
+const router = createRouter({ routeTree, context: {queryClient} })
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
@@ -21,8 +23,6 @@ declare module '@tanstack/react-router' {
     router: typeof router
   }
 }
-
-const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
