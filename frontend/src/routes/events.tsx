875daaa2f9f3b9870b1df287fd8dea7ed161d0d6
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-06-27 01:37:30 -0500

    optimized delete functionality

--- frontend/src/routes/events.tsx
+++ frontend/src/routes/events.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { eventQueryOptions, loadingCreateExperienceQueryOptions } from '@/lib/api'
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 import { useMutation } from '@tanstack/react-query'
 
 import { deleteExperience } from '@/lib/api'
@@ -48,6 +48,7 @@ function StyledEvents() {
 }
 
 function ExperienceDeleteButton({id} : { id: number }) {
+  const queryClient = useQueryClient()
 
   const mutation = useMutation({
     mutationFn: deleteExperience,
@@ -58,13 +59,23 @@ function ExperienceDeleteButton({id} : { id: number }) {
     },
     onSuccess: () => {
       toast('Success', {
-        description: `${id} has been deleted`
+        description: `Experience has been deleted`
       })
+
+      queryClient.setQueryData(
+        eventQueryOptions.queryKey,
+        (existingExperiences) => ({
+          ...existingExperiences,
+          experiences: existingExperiences!.experiences.filter((e) => e.id !== id),
+        })
+      )
+
     }
   })
 
   return (
     <Button
+      disabled={mutation.isPending}
       onClick={() => mutation.mutate({ id })}
       variant='outline'
     >
