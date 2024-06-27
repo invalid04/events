638fabf4e851895c9acd8c1a1adc62c8a52171bd
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-06-27 04:49:57 -0500

    successfully mapped over myEvent data

--- frontend/src/components/Custom/MyEventCard.tsx
+++ frontend/src/components/Custom/MyEventCard.tsx
@@ -3,9 +3,24 @@ import { CardTitle } from "../ui/card";
 
 import { useQuery } from "@tanstack/react-query";
 
+import { myEventQueryOptions } from "@/lib/api";
+
 export function MyEventCard() {
 
+    const { isPending, error, data } = useQuery(myEventQueryOptions)
+
+    if (isPending) return '...'
+    if (error) return 'error'
+
     return (
-        <h2>no</h2>
+        <div>
+            {data.experiences.map((experience) => (
+                <div key={experience.id}>
+                    <p>{experience.title}</p>
+                    <p>{experience.maxAttendance}</p>
+                    <p>{experience.date}</p>
+                </div>
+            ))}
+        </div>
     )
 }
