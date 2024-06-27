86bab3d3bac6fc6aaed5cf6bfa85cb2a6f8865cd
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-06-27 05:27:07 -0500

    finished profile page styling

--- frontend/src/routes/_authenticated/profile.tsx
+++ frontend/src/routes/_authenticated/profile.tsx
@@ -9,9 +9,14 @@ export const Route = createFileRoute('/_authenticated/profile')({
 
 function ProfilePage() {
   return (
-    <div>
-      <ProfileCard />
-      <MyEventCard />
-    </div>
-  )
+      <div className="flex flex-col">
+          <div className="mb-6 md:mb-0 md:mr-6 md:w-1/3">
+              <ProfileCard />
+          </div>
+          <div className="flex-1">
+              <MyEventCard />
+          </div>
+      </div>
+  );
 }
+
