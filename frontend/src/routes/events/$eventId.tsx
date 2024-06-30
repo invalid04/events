2f9a92dd613d50036d099e3083ee032f63010776
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-06-30 00:53:13 -0500

    created Event interface in EventComponent page

--- frontend/src/routes/events/$eventId.tsx
+++ frontend/src/routes/events/$eventId.tsx
@@ -1,5 +1,28 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, useParams } from '@tanstack/react-router'
+
+import { eventByIdQueryOptions } from '@/lib/api'
+import { useQuery } from '@tanstack/react-query'
 
 export const Route = createFileRoute('/events/$eventId')({
-  component: () => <div>Hello /events/$eventId!</div>
+  component: () => <div>Hello /events/$eventId!</div>,
+
 })
+
+interface Event {
+  id: number,
+  title: string,
+  description: string,
+  location: string,
+  maxAttendees: string
+}
+
+const EventComponent = () => {
+  const { eventId } = useParams({ from: '/events/$eventId'})
+
+  const { isPending, error, data } = useQuery(eventByIdQueryOptions(Number(eventId)))
+
+  if (isPending) return '...'
+  if (error) return 'error'
+
+  const event: Event | undefined = data as Event
+}
